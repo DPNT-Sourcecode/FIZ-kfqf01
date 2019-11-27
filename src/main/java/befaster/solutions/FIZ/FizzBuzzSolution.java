010613d91bbbaf968e7083cfaf34ab9c32a846f2
sbx_user1051 <sbx_user1051@169.254.39.165>
2019-11-27 16:59:23 +0000

    Wed Nov 27 16:59:23 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -3,11 +3,34 @@ package befaster.solutions.FIZ;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        if(number%3 == 0 && number%15 != 0)
+        int cond3 = 0;
+        int cond5 = 0;
+        int ok1 = 0, ok2 = 0, ok3 = 0;
+        int copy = number;
+
+        while(copy != 0){
+            if(copy%10 == 3)
+                cond3 = 1;
+            if(copy%10 == 5)
+                cond5 = 1;
+            copy = copy/10;
+        }
+        System.out.println(cond3 + " " + cond5);
+
+        if(number%3 == 0 || cond3 == 1)
+            ok1 = 1;
+        if(number%5 == 0 || cond5 == 1)
+            ok2 = 1;
+        if((number%5 == 0 || cond5 == 1)&&(number%3 == 0 || cond3 == 1))
+            ok3 = 1;
+
+        System.out.println(ok1 +" "+ ok2);
+
+        if(ok1 == 1)
             return "fizz";
-        else if(number%5 == 0 && number%15 != 0)
+        else if(ok2 == 1)
             return "buzz";
-        else if(number%15 == 0)
+        else if(ok1 == 1 && ok2 == 1)
             return "fizz buzz";
         else
             return String.valueOf(number);
@@ -15,8 +38,9 @@ public class FizzBuzzSolution {
 
     public static void main(String[] args){
         FizzBuzzSolution sol = new FizzBuzzSolution();
-        String result = sol.fizzBuzz(29);
+        String result = sol.fizzBuzz(30);
         System.out.println("The result is:" + result);
     }
 
 }
+
