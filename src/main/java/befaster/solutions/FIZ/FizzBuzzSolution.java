599cff385a21dcb1f3a0ff9d6c6e1805a027e3ba
sbx_user1051 <sbx_user1051@169.254.39.165>
2019-11-27 17:19:07 +0000

    Wed Nov 27 17:19:07 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -5,7 +5,9 @@ public class FizzBuzzSolution {
     public String fizzBuzz(Integer number) {
         int cond3 = 0;
         int cond5 = 0;
+        int condid = 1;
         int copy = number;
+        int deluxe = 0;
 
         while (copy != 0) {
             if (copy % 10 == 3)
@@ -15,24 +17,42 @@ public class FizzBuzzSolution {
             copy = copy / 10;
         }
 
+        copy = number;
 
-        if ((number % 3 == 0 || cond3 == 1) && (number % 5 == 0 || cond5 == 1))
+        while(copy>10){
+            if (copy % 10 != ((copy / 10) % 10)) {
+                condid = 0;
+                break;
+            }
+            copy = copy/10;
+        }
+
+        if(number>10 && condid == 1)
+            deluxe = 1;
+
+
+        if((number % 3 == 0 || cond3 == 1) && (number % 5 == 0 || cond5 == 1) && deluxe == 1)
+            return "fizz buzz deluxe";
+        else if ((number % 3 == 0 || cond3 == 1) && (number % 5 == 0 || cond5 == 1))
             return "fizz buzz";
+        else if((number % 3 == 0 || cond3 == 1) && deluxe == 1)
+            return "fizz deluxe";
+        else if((number % 5 == 0 || cond5 == 1) && deluxe == 1)
+            return "buzz deluxe";
         else if (number % 5 == 0 || cond5 == 1)
             return "buzz";
         else if (number % 3 == 0 || cond3 == 1)
             return "fizz";
+        else if (deluxe == 1)
+            return "deluxe";
         else
             return String.valueOf(number);
     }
 
     public static void main(String[] args){
         FizzBuzzSolution sol = new FizzBuzzSolution();
-        String result = sol.fizzBuzz(30);
+        String result = sol.fizzBuzz(53);
         System.out.println("The result is:" + result);
     }
 
 }
-
-
-
